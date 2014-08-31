abf8de7f51641c287581c95a5473a581e1ee074e
Kusnierewicz <kusnierewicz@gmail.com>
2014-09-01 01:02:00 +0200

    game-over method finished

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -29,5 +29,45 @@ module TicTacToe
   	  end
   	end
 
+  	context "#get_cell" do
+  	  it "returns the cell based on the (x, y) coordinate" do
+  	  	grid = [["", "", ""], ["", "", "something"], ["", "", ""]]
+  	  	board = Board.new(grid: grid)
+  	  	expect(board.get_cell(2, 1)).to eq "something"
+  	  end
+  	end
+
+  	context "#set_cell" do
+  	  it "updates the value of hte cat a (x, y) coordinate" do
+  	  	Cat = Struct.new(:value)
+  	  	grid = [[Cat.new("cool"), "", ""], ["", "", ""], ["", "", ""]]
+  	  	board = Board.new(grid: grid)
+  	  	board.set_cell(0,0, "meow")
+  	  	expect(board.get_cell(0,0).value).to eq "meow"
+  	  end
+  	end
+
+  	context "#game_over" do
+  	  it "returns :winner if winner? is true" do
+  	  	board = Board.new
+  	  	board.stub(:winner?) { true }
+  	  	expect(board.game_over).to eq :winner
+  	  end
+
+  	  it "returns :draw if winner? is false and draw? is true" do
+  	  	board = Board.new
+  	  	board.stub(:winner?) { false }
+  	  	board.stub(:draw?) { true }
+  	  	expect(board.game_over).to eq :draw
+  	  end
+
+  	  it "returns false if winner? is false and draw? is false" do
+  	  	board = Board.new
+  	  	board.stub(:winner?) { false }
+  	  	board.stub(:draw?) { false }
+  	  	expect(board.game_over).to eq false
+  	  end
+  	end
+
   end
 end
